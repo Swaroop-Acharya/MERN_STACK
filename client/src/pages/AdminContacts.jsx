89b90ce8aaa9e2c23c3df17a2cb302ebc137aef3
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2024-04-10 11:24:19 +0530

    Done : Loading animation to all the pages

--- client/src/pages/AdminContacts.jsx
+++ client/src/pages/AdminContacts.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../store/auth";
 import { toast } from "react-toastify";
+import ReactLoading from "react-loading";
+
 export default function AdminContacts() {
   const [contacts, setContacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const { autherizationToken } = useAuth();
+
   const getAllContacts = async () => {
+    setIsLoading(true); // Set loading state to true before fetching contacts
     try {
       const URL = "https://mern-stack-server-nine.vercel.app/api/admin/contacts";
       const response = await fetch(URL, {
@@ -26,6 +31,8 @@ export default function AdminContacts() {
         "Something went wrong while fetching contacts for admin",
         error
       );
+    } finally {
+      setIsLoading(false); // Set loading state to false after fetching contacts
     }
   };
 
@@ -34,6 +41,7 @@ export default function AdminContacts() {
   }, []);
 
   const handleDeleteContact = async (id) => {
+    setIsLoading(true); // Set loading state to true before deleting contact
     try {
       const URL = `https://mern-stack-server-nine.vercel.app/api/admin/contacts/delete/${id}`;
       const response = await fetch(URL, {
@@ -57,26 +65,32 @@ export default function AdminContacts() {
     <>
       <h1 className="text-3xl text-white p-3">List of Messages</h1>
       <div className="" style={{ height: "450px", overflowY: "auto" }}>
-        {contacts.map((contact) => (
-          <div
-            key={contact.id}
-            className="mb-20px p-8 bg-[#31363F] text-white  mb-2 flex justify-between  rounded"
-          >
-            <div>
-              <h4 className="text-xl font-bold">{contact.username}</h4>
-              <p className="text-sm">{contact.email}</p>
-              <p className="text-sm">{contact.message}</p>
-            </div>
-            <div>
-              <button
-                className="mt-2 px-4 py-2 bg-red-500 text-white rounded"
-                onClick={() => handleDeleteContact(contact._id)}
-              >
-                Delete
-              </button>
-            </div>
+        {isLoading ? ( // Display loading indicator while fetching or deleting
+          <div className="flex justify-center items-center h-80">
+            <ReactLoading type={"spin"} color={"white"} height={50} width={50} />
           </div>
-        ))}
+        ) : (
+          contacts.map((contact) => (
+            <div
+              key={contact.id}
+              className="mb-20px p-8 bg-[#31363F] text-white  mb-2 flex justify-between  rounded"
+            >
+              <div>
+                <h4 className="text-xl font-bold">{contact.username}</h4>
+                <p className="text-sm">{contact.email}</p>
+                <p className="text-sm">{contact.message}</p>
+              </div>
+              <div>
+                <button
+                  className="mt-2 px-4 py-2 bg-red-500 text-white rounded"
+                  onClick={() => handleDeleteContact(contact._id)}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </>
   );
--- client/src/pages/AdminProjects.jsx
+++ client/src/pages/AdminProjects.jsx
@@ -4,9 +4,13 @@ import { Badge } from "@/components/ui/badge";
 import { FaGithub } from "react-icons/fa";
 import { useAuth } from "@/store/auth";
 import { toast } from "react-toastify";
+import ReactLoading from "react-loading";
+
 export default function AdminProjects() {
   const [projects, setProjects] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const { autherizationToken } = useAuth();
+
   const getAllProjects = async () => {
     try {
       const URL = `https://mern-stack-server-nine.vercel.app/api/admin/projects`;
@@ -18,14 +22,17 @@ export default function AdminProjects() {
       });
       if (response.ok) {
         const data = await response.json();
-        setProjects(data); // Assuming your response directly contains the projects array
+        setProjects(data);
       }
     } catch (error) {
       console.log("Something went wrong while fetching projects!!");
+    } finally {
+      setIsLoading(false); // Set loading state to false after fetching
     }
   };
 
   const handleDelete = async (id) => {
+    setIsLoading(true); // Set loading state to true before deletion
     try {
       const URL = `https://mern-stack-server-nine.vercel.app/api/admin/projects/delete/${id}`;
       const response = await fetch(URL, {
@@ -61,49 +68,55 @@ export default function AdminProjects() {
         </Link>
       </div>
       <div className="mt-8" style={{ height: "400px", overflowY: "auto" }}>
-        {projects.map((project) => (
-          <div
-            key={project._id}
-            className="bg-[#31363F] text-white p-4 flex items-center justify-between rounded mb-4"
-          >
-            <div className="flex flex-col gap-2">
-              <div className="flex  items-center gap-2">
-                <h2 className="text-2xl font-bold">{project.name}</h2>
-                <a
-                  href={project.gitHubLink}
-                  target="_blank"
-                  className="block"
-                  rel="noopener noreferrer"
-                >
-                  <FaGithub />
-                </a>
-              </div>
-
-              <div className="flex gap-2">
-                {project.techStack.map((tech) => (
-                  <Badge key={tech}>{tech}</Badge>
-                ))}
-              </div>
-              <p className="text-slate-300 w-4/5 text-justify">
-                {project.description}
-              </p>
-            </div>
-            <div className="mt-4 flex flex-col gap-2">
-              <Link
-                to={`/admin/projects/${project._id}/edit`}
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2"
-              >
-                Edit
-              </Link>
-              <button
-                onClick={() => handleDelete(project._id)}
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              >
-                Delete
-              </button>
-            </div>
+        {isLoading ? ( // Display loading indicator while fetching or deleting
+          <div className="flex justify-center items-center h-64">
+            <ReactLoading type={"spin"} color={"white"} height={50} width={50} />
           </div>
-        ))}
+        ) : (
+          projects.map((project) => (
+            <div
+              key={project._id}
+              className="bg-[#31363F] text-white p-4 flex items-center justify-between rounded mb-4"
+            >
+              <div className="flex flex-col gap-2">
+                <div className="flex  items-center gap-2">
+                  <h2 className="text-2xl font-bold">{project.name}</h2>
+                  <a
+                    href={project.gitHubLink}
+                    target="_blank"
+                    className="block"
+                    rel="noopener noreferrer"
+                  >
+                    <FaGithub />
+                  </a>
+                </div>
+
+                <div className="flex gap-2">
+                  {project.techStack.map((tech) => (
+                    <Badge key={tech}>{tech}</Badge>
+                  ))}
+                </div>
+                <p className="text-slate-300 w-4/5 text-justify">
+                  {project.description}
+                </p>
+              </div>
+              <div className="mt-4 flex flex-col gap-2">
+                <Link
+                  to={`/admin/projects/${project._id}/edit`}
+                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2"
+                >
+                  Edit
+                </Link>
+                <button
+                  onClick={() => handleDelete(project._id)}
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );

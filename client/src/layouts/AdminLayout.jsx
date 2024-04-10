c9ac4dbee4c254e41a93f1b949fcde338fa2d0dc
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2024-04-10 15:10:04 +0530

    changes

--- client/src/layouts/AdminLayout.jsx
+++ client/src/layouts/AdminLayout.jsx
@@ -9,17 +9,17 @@ export default function AdminLayout() {
   const navigate = useNavigate();
   const location = useLocation();
 
-  // if (isLoading) {
-  //   return (
-  //     <div className="flex items-center justify-center h-[75vh]">
-  //       <ReactLoading type={"bars"} color={"white"} height={60} width={60} />
-  //     </div>
-  //   );
-  // }
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-[75vh]">
+        <ReactLoading type={"bars"} color={"white"} height={60} width={60} />
+      </div>
+    );
+  }
 
-  // if (!user.isAdmin) {
-  //   navigate("/");
-  // }
+  if (!user.isAdmin) {
+    navigate("/");
+  }
 
   return (
     <div className="flex flex-col h-[89vh]">

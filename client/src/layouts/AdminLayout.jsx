294cca96033e2911df375247dc91f2cd9ac3e1a5
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2024-04-10 10:34:59 +0530

    changes

--- client/src/layouts/AdminLayout.jsx
+++ client/src/layouts/AdminLayout.jsx
@@ -11,7 +11,7 @@ export default function AdminLayout() {
 
   if (isLoading) {
     return (
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center">
         <ReactLoading
           type={"bars"}
           color={"white"}

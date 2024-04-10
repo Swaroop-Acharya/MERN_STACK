56201454ae35b8d6239c0d3ef58e50580e53ba1d
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2024-04-10 11:39:24 +0530

    Changes

--- client/src/layouts/AdminLayout.jsx
+++ client/src/layouts/AdminLayout.jsx
@@ -22,55 +22,60 @@ export default function AdminLayout() {
   // }
 
   return (
-    <div className="flex h-[90vh]">
-      <nav className="bg-gray-200 w-48 p-4 h-full flex flex-col justify-between">
-        <ul className="flex flex-col">
-          <li className="mb-2">
-            <Link
-              to="/admin/home"
-              className={`text-gray-800 font-semibold hover:text-gray-900 ${
-                location.pathname === "/admin/home" && "bg-gray-300 pr-10"
-              }`}
-            >
-              Home
-            </Link>
-          </li>
-          <li className="mb-2">
-            <Link
-              to="/admin/users"
-              className={`text-gray-800 font-semibold hover:text-gray-900 ${
-                location.pathname === "/admin/users" && "bg-gray-300 pr-10"
-              }`}
-            >
-              Users
-            </Link>
-          </li>
-          <li className="mb-2">
-            <Link
-              to="/admin/projects"
-              className={`text-gray-800 font-semibold hover:text-gray-900 ${
-                location.pathname === "/admin/projects" && "bg-gray-300 pr-10"
-              }`}
-            >
-              Projects
-            </Link>
-          </li>
-          <li className="mb-2">
-            <Link
-              to="/admin/contacts"
-              className={`text-gray-800 font-semibold hover:text-gray-900 ${
-                location.pathname === "/admin/contacts" && "bg-gray-300 pr-10"
-              }`}
-            >
-              Contacts
-            </Link>
-          </li>
-        </ul>
-      </nav>
-
-      <div className="flex-1 justify-center items-center p-4 h-[90vh]  bg-[#222831]">
-        <Outlet />
+    <div className="flex flex-col h-[89vh]">
+      <div className="flex-1 flex h-[80vh]">
+        <nav className="bg-gray-200 w-48 p-4 h-full flex flex-col justify-between">
+          <ul className="flex flex-col">
+            <li className="mb-2">
+              <Link
+                to="/admin/home"
+                className={`text-gray-800 font-semibold hover:text-gray-900 ${
+                  location.pathname === "/admin/home" && "bg-gray-300 pr-10"
+                }`}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link
+                to="/admin/users"
+                className={`text-gray-800 font-semibold hover:text-gray-900 ${
+                  location.pathname === "/admin/users" && "bg-gray-300 pr-10"
+                }`}
+              >
+                Users
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link
+                to="/admin/projects"
+                className={`text-gray-800 font-semibold hover:text-gray-900 ${
+                  location.pathname === "/admin/projects" && "bg-gray-300 pr-10"
+                }`}
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link
+                to="/admin/contacts"
+                className={`text-gray-800 font-semibold hover:text-gray-900 ${
+                  location.pathname === "/admin/contacts" && "bg-gray-300 pr-10"
+                }`}
+              >
+                Contacts
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex-1 justify-center items-center p-4 h-[full]  bg-[#222831]">
+          <Outlet />
+        </div>
       </div>
+      {/* Footer */}
+      <footer className="bg-white py-2 text-center text-gray-800">
+        Designed & Developed by Swaroop 💖
+      </footer>
     </div>
   );
 }
